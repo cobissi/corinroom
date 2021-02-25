c7349b6279ac125d24cc03a4074219bdc0093b7b
Cobi <Cobi@DESKTOP-LJ47UNE>
2021-02-25 13:47:41 +0900

    첫번째 커밋(저장)

---
+++ corinRoom/src/com/git/corinRoom/Member.java
@@ -0,0 +1,15 @@
+package com.git.corinRoom;
+
+
+
+public class Member {
+	// 클래스 멤버변수
+	String name;			// 이름(문자열)
+	char gender;			// 셩별(F/M)
+	String dogType;			// 견종(단어)
+	String bitrhDay;		// 생년월일
+	int weight;				// 몸무게
+	boolean allergy;		// 알레르기 여부
+	boolean neutralization;	// 중성화 여부
+	
+}
